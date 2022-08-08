c757b73a3a96dc9fadc68d63205024254ff3ad9b
murin <murin.stanislav@mail.ru>
2022-08-08 15:17:09 +0300

    Initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,8 @@
+public class FormDate {
+
+    public int day;// добавляю поле ДЕНЬ с типом int
+    public int month;// добавляю поле МЕСЯЦ с типом int
+    public int year;// добавляю поле ГОД с типом int
+}
+
+
---
+++ src/main/java/Post.java
@@ -0,0 +1,16 @@
+
+public class Post {
+
+    public String name;// добавляю поле ИМЯ с типом String
+    public String passport;// добавляю поле ПАСПОРТ с типом String
+    public String patronymic;// добавляю поле ОТЧЕСТВО с типом String
+    public String phone;// добавляю поле №ТЕЛЕФОНА с типом String
+    public String surname;// добавляю поле ФАМИЛИЯ с типом String
+    public boolean subscription;// добавляю поле ПОДПИСКА с типом boolean(true/false)
+
+    public FormDate birthday; //в классе Post добавляем поле birthday с типом FormDate
+
+
+}
+
+
